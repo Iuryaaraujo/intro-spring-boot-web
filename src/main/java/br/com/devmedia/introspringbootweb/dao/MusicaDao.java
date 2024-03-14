36f927b4194edf7ad6055a45db5687e95c904b34
Iury Araujo <iurymiraima@gmail.com>
2024-03-14 14:35:36 -0300

    A página de listagem de playlists

--- src/main/java/br/com/devmedia/introspringbootweb/dao/MusicaDao.java
+++ src/main/java/br/com/devmedia/introspringbootweb/dao/MusicaDao.java
@@ -7,8 +7,7 @@ import java.util.List;
 public interface MusicaDao {
     void salvar(Musica musica);
     List<Musica> recuperarPorPlaylist(long playlistId);
-    Musica recuperarPorPlaylistIdEMusicaId
-            (long playlistId, long musicaId);
+    Musica recuperarPorPlaylistIdEMusicaId(long playlistId, long musicaId);
     void atualizar(Musica musica);
     void excluir(long musicaId);
 }
